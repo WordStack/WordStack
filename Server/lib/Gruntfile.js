7379c406c7920d6cb14f64d8d908948fd6f39b2f
Preta-Crowz <preta@siro.dev>
2022-10-13 09:31:44 +0900

    grunt 관련 업데이트

--- Server/lib/Gruntfile.js
+++ Server/lib/Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function(grunt){
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	
 	grunt.registerTask('default', ['concat', 'uglify']);
